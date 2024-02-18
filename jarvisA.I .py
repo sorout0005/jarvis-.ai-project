a6db5559e769e9ae710d2adc1649570af598b2ab
gautam sorout <142593022+sorout0005@users.noreply.github.com>
2024-02-18 11:04:46 +0530

    Add files via upload
    this is modified

--- jarvisA.I .py
+++ jarvisA.I .py
@@ -1,30 +1,44 @@
 import speech_recognition as sr
-import os
 import pyttsx3 as pyt
-
-
+import webbrowser as wb
 
 speaker = pyt.init("sapi5")
+voices = speaker.getProperty("voices")
+speaker.setProperty('voices',voices[0].id)
+speaker.setProperty('rate', 120)
 
 
 
-def speak (text):
-    pyt.say("dfgh")
+def say (text):
+    pyt.speak(f"{text}")
 
 
 def takeCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        r.pause_threshold = 1
+        r.pause_threshold = 0.6
         audio = r.listen(source)
-        query = r.recognize_google(audio, language='en-in')
-        print(f"You said: {query}")
+        try:
+            print("Recognizing....")
+            query = r.recognize_google(audio, language='en-in')
+            print(f"You said: {query}")
+            return query
+        except Exception as e:
+            print("some error occured while listening please try again")
 if __name__ == '__main__':
-    print('PyCharm')
-    speak("Hello I am JArvis A.I")
-    print('I am listening...')
-    text = takeCommand()
-    speak(text)
+    print('started')
+    say("Hello I am JArvis AI")
+    while True:
+        print('I am listening...')
+        text = takeCommand()
+        sites = [["youtube","https://www.youtube.com"],["wikipedia","https://www.wikipedia.com"],
+                 ["Google","https://www.google.com"],["instagram","https://www.instagram.com"],
+                 ["facebook","https://www.facebook.com"],["spotify","https://www.spotify.com"]]
+        for site in sites:
+            if f"Open {site[0]}".lower() in text.lower():
+                say(f"Opening{site[0]} Sir")
+                wb.open(site[1])
+        #say(text)
     
 speaker.runAndWait()
 
